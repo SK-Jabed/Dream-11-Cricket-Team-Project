ad5451f4d1d38f2a7804fa43606adddf8b9977aa
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-24 23:35:22 +0600

    Banner Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Banner from "./components/Header/Banner";
 import Navbar from "./components/Header/Navbar";
 
@@ -7,7 +8,14 @@ const App = () => {
       {/* Header Section Starts Here */}
       <Navbar></Navbar>
       <Banner></Banner>
+      
       {/* Header Section Ends Here */}
+      {/* Main Section Starts Here */}
+      
+      {/* Main Section Ends Here */}
+      {/* Footer Section Starts Here */}
+      <Footer></Footer>
+      {/* Footer Section Ends Here */}
     </div>
   );
 };
