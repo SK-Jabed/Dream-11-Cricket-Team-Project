24bd021504a9b463ece2cbc2f819bdce7a7d9656
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-13 23:24:58 +0600

    Banner and Available Players Component File Updated

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -1,23 +1,41 @@
 import PropTypes from "prop-types";
-import bannerImage from "../../assets/banner-main.png"
+import bannerImage from "../../assets/banner-main.png";
 
-const Banner = ({handleCreditCoin}) => {
-    return (
-        <div className="container w-11/12 mx-auto bg-cover bg-black bg-no-repeat rounded-3xl md:h-[32rem] mb-8 mt-8" style={{backgroundImage: "url(https://i.ibb.co.com/fvVDKf7/bg-shadow.png)"}}>
-            <div className="flex flex-col items-center justify-center text-center">
-                <img className="mt-6 md:mt-8 lg:mt-12 mb-3 md:mb-4 lg:mb-6" src={bannerImage} alt="Banner Image"></img>
-                <h1 className="text-xl md:text-2xl lg:text-4xl font-extrabold text-white">Assemble Your Ultimate Dream 11 Cricket Team</h1>
-                <p className="text-sm md:text-lg lg:text-xl font-semibold text-gray-400 mt-2 md:mt-4">Beyond Boundaries Beyond Limits</p>
-                <div className="mt-2 mb-2 md:mt-6 lg:mt-8 border-2 border-solid border-yellow-300 rounded-xl p-1">
-                <button onClick={handleCreditCoin} className="bg-yellow-400 rounded-xl py-3 px-5 hover:bg-yellow-500"><a className="text-lg font-bold">Claim Free Credit</a></button>
-                </div>
-            </div>
+const Banner = ({ handleCreditCoin }) => {
+  return (
+    <div
+      className="container w-11/12 mx-auto bg-cover bg-black bg-no-repeat rounded-3xl md:h-[32rem] mb-8 mt-8"
+      style={{
+        backgroundImage: "url(https://i.ibb.co.com/fvVDKf7/bg-shadow.png)",
+      }}
+    >
+      <div className="flex flex-col items-center justify-center text-center">
+        <img
+          className="mt-6 md:mt-8 lg:mt-12 mb-3 md:mb-4 lg:mb-6"
+          src={bannerImage}
+          alt="Banner Image"
+        ></img>
+        <h1 className="text-xl md:text-2xl lg:text-4xl font-extrabold text-white">
+          Assemble Your Ultimate Dream 11 Cricket Team
+        </h1>
+        <p className="text-sm md:text-lg lg:text-xl font-semibold text-gray-400 mt-2 md:mt-4">
+          Beyond Boundaries Beyond Limits
+        </p>
+        <div className="mt-2 mb-2 md:mt-6 lg:mt-8 border-2 border-solid border-yellow-300 rounded-xl p-1">
+          <button
+            onClick={handleCreditCoin}
+            className="bg-yellow-400 rounded-xl py-3 px-5 hover:bg-yellow-500"
+          >
+            <a className="text-lg font-bold">Claim Free Credit</a>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 Banner.propTypes = {
-    handleCreditCoin: PropTypes.func
-}
+  handleCreditCoin: PropTypes.func,
+};
 
 export default Banner;
--- src/components/Main/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/Main/AvailablePlayers/AvailablePlayers.jsx
@@ -1,100 +1,120 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import batsman_logo from "/src/assets/cricket.png"
-import bowler_logo from "/src/assets/shot.png"
-import rating_logo from "/src/assets/badge.png"
-import price_logo from "/src/assets/affordable.png"
-import player_rating_logo from "/src/assets/star.png"
+import batsman_logo from "/src/assets/cricket.png";
+import bowler_logo from "/src/assets/shot.png";
+import rating_logo from "/src/assets/badge.png";
+import price_logo from "/src/assets/affordable.png";
+import player_rating_logo from "/src/assets/star.png";
 
+const AvailablePlayers = ({ addPlayerToQueue }) => {
+  const [allPlayers, setAllPlayers] = useState([]);
 
-const AvailablePlayers = ({addPlayerToQueue}) => {
-    const [allPlayers, setAllPlayers] = useState([]);
+  useEffect(() => {
+    fetch(`players.json`)
+      .then((res) => res.json())
+      .then((data) => setAllPlayers(data));
+  }, []);
 
-    useEffect(() => {
-        fetch(`players.json`)
-            .then(res => res.json())
-            .then(data => setAllPlayers(data))
-    }, [])
+  return (
+    <div className="container mx-auto mt-4 lg:mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
+        {allPlayers.map((player) => (
+          <div key={player.player_id} className="card shadow-2xl p-4">
+            <figure>
+              <img
+                className="w-[28rem] h-[18rem] rounded-xl"
+                src={player.player_image}
+                alt="Player Image"
+              />
+            </figure>
+            <div className="">
+              <div className="flex items-center gap-2 mt-4 lg:mt-6">
+                <img
+                  className="w-14 h-14 border-2 border-solid rounded-full object-cover"
+                  src={player.player_profile}
+                ></img>
+                <h2 className="plyer-name text-lg md:text-xl lg:text-2xl font-semibold">
+                  {player.player_name}
+                </h2>
+              </div>
+              <div className="flex justify-between items-center mt-1 border-b-2 pb-2">
+                <div className="flex items-center gap-2 justify-center">
+                  <i className="fa-solid fa-flag text-lg text-gray-600"></i>
+                  <div className="flex items-center gap-1">
+                    <p className="text-sm md:text-base lg:text-lg font-semibold text-gray-600">
+                      {player.national_team}
+                    </p>
+                    <img
+                      className="w-8 h-8 border-2 border-solid rounded-full object-cover"
+                      src={player.national_flag}
+                    ></img>
+                  </div>
+                </div>
 
-    return (
-        <div className="container mx-auto mt-4 lg:mt-8">
-            
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
-                {
-                    allPlayers.map(player => (
-                    <div key={player.player_id} className="card shadow-2xl p-4">
-                        <figure>
-                            <img className="w-[28rem] h-[18rem] rounded-xl"
-                                src={player.player_image}
-                                alt="Player Image" />
-                            </figure>
-                        <div className="">
-                            <div className="flex items-center gap-2 mt-4 lg:mt-6">
-                                <img className="w-14 h-14 border-2 border-solid rounded-full object-cover" src={player.player_profile}></img>
-                                <h2 className="plyer-name text-lg md:text-xl lg:text-2xl font-semibold">{player.player_name}</h2>
-                            </div>
-                            <div className="flex justify-between items-center mt-1 border-b-2 pb-2">
-                                <div className="flex items-center gap-2 justify-center">
-                                    <i className="fa-solid fa-flag text-lg text-gray-600"></i>
-                                    <div className="flex items-center gap-1">
-                                    <p className="text-sm md:text-base lg:text-lg font-semibold text-gray-600">{player.national_team}</p>
-                                    <img className="w-8 h-8 border-2 border-solid rounded-full object-cover" src={player.national_flag}></img>
-                                    </div>
-                                </div>
+                <button
+                  type="button"
+                  className="btn group flex h-12 w-36 items-center justify-center rounded-lg bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 p-[1.5px] text-white duration-300 hover:bg-gradient-to-l hover:shadow-2xl hover:shadow-purple-600/30"
+                >
+                  <div className="flex h-full w-full items-center justify-center rounded-lg bg-gray-900 transition duration-300 ease-in-out group-hover:bg-gradient-to-br group-hover:from-gray-700 group-hover:to-gray-900 group-hover:transition group-hover:duration-300 group-hover:ease-in-out">
+                    {player.player_role}
+                  </div>
+                </button>
+              </div>
+              <div className="flex justify-between text-lg font-semibold items-center mt-4">
+                <div className="flex items-center gap-1">
+                  <img className="w-6" src={rating_logo}></img>
+                  <p className="">Rating:</p>
+                </div>
+                <div className="flex items-center gap-1 justify-center">
+                  <p className="">{player.rating}</p>
+                  <img className="w-5" src={player_rating_logo}></img>
+                </div>
+              </div>
+              <div className="flex justify-between text-base font-semibold items-center mt-2">
+                <div className="flex items-center">
+                  <img className="w-8" src={batsman_logo}></img>
+                  <p>Batting Style:</p>
+                </div>
+                <p className="text-gray-500">{player.batting_style}</p>
+              </div>
+              <div className="flex justify-between text-base font-semibold items-center mt-1 border-b-2 pb-2">
+                <div className="flex items-center gap-1">
+                  <img className="w-7" src={bowler_logo}></img>
+                  <p>Bowling Style:</p>
+                </div>
+                <p className="text-gray-600">{player.bowling_style}</p>
+              </div>
 
-                                <button type="button" className="btn group flex h-12 w-36 items-center justify-center rounded-lg bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 p-[1.5px] text-white duration-300 hover:bg-gradient-to-l hover:shadow-2xl hover:shadow-purple-600/30">
-                                    
-                                    <div className="flex h-full w-full items-center justify-center rounded-lg bg-gray-900 transition duration-300 ease-in-out group-hover:bg-gradient-to-br group-hover:from-gray-700 group-hover:to-gray-900 group-hover:transition group-hover:duration-300 group-hover:ease-in-out">{player.player_role}</div>
-                                </button>
-                            </div>
-                            <div className="flex justify-between text-lg font-semibold items-center mt-4">
-                                <div className="flex items-center gap-1">
-                                    <img className="w-6" src={rating_logo}></img>
-                                    <p className="">Rating:</p>
-                                </div>
-                                <div className="flex items-center gap-1 justify-center">
-                                <p className="">{player.rating}</p>
-                                <img className="w-5" src={player_rating_logo}></img>
-                                </div>
-                            </div>
-                            <div className="flex justify-between text-base font-semibold items-center mt-2">
-                                <div className="flex items-center"> 
-                                <img className="w-8" src={batsman_logo}></img>
-                                <p>Batting Style:</p>
-                                </div>
-                                <p className="text-gray-500">{player.batting_style}</p>
-                            </div>    
-                            <div className="flex justify-between text-base font-semibold items-center mt-1 border-b-2 pb-2">
-                                <div className="flex items-center gap-1"> 
-                                <img className="w-7" src={bowler_logo}></img>
-                                <p>Bowling Style:</p>
-                                </div>                               
-                                <p className="text-gray-600">{player.bowling_style}</p>
-                            </div>                                  
-                            
-                            <div className="flex justify-between items-center pt-2 pb-2 border-b-2">
-                                <div className="flex items-center gap-1"> 
-                                <img className="w-7" src={price_logo}></img>
-                                <p className="text-lg font-bold">Price:</p>                               
-                                </div>
-                                <p className="text-gray-800 text-lg font-bold">${player.bidding_price}</p>                               
-                            </div>
-                            <div className="flex justify-center items-center pt-2">
-                            <button onClick={() => addPlayerToQueue(player)} type="button" className=" group flex h-14 w-full items-center justify-center rounded-lg bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 p-[1.5px] text-white duration-300 hover:bg-gradient-to-l hover:shadow-2xl hover:shadow-purple-600/30">                                    
-                                <div className="flex h-full w-full items-center justify-center rounded-lg bg-gray-900 transition duration-300 ease-in-out group-hover:bg-gradient-to-br group-hover:from-gray-700 group-hover:to-gray-900 group-hover:transition group-hover:duration-300 group-hover:ease-in-out text-lg font-semibold">Choose Player</div>
-                            </button>
-                            </div>
-                        </div>
-                    </div>
-                ))
-                }
+              <div className="flex justify-between items-center pt-2 pb-2 border-b-2">
+                <div className="flex items-center gap-1">
+                  <img className="w-7" src={price_logo}></img>
+                  <p className="text-lg font-bold">Price:</p>
+                </div>
+                <p className="text-gray-800 text-lg font-bold">
+                  ${player.bidding_price}
+                </p>
+              </div>
+              <div className="flex justify-center items-center pt-2">
+                <button
+                  onClick={() => addPlayerToQueue(player)}
+                  type="button"
+                  className=" group flex h-14 w-full items-center justify-center rounded-lg bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 p-[1.5px] text-white duration-300 hover:bg-gradient-to-l hover:shadow-2xl hover:shadow-purple-600/30"
+                >
+                  <div className="flex h-full w-full items-center justify-center rounded-lg bg-gray-900 transition duration-300 ease-in-out group-hover:bg-gradient-to-br group-hover:from-gray-700 group-hover:to-gray-900 group-hover:transition group-hover:duration-300 group-hover:ease-in-out text-lg font-semibold">
+                    Choose Player
+                  </div>
+                </button>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 AvailablePlayers.propTypes = {
-    addPlayerToQueue: PropTypes.func
-}
+  addPlayerToQueue: PropTypes.func,
+};
 
 export default AvailablePlayers;
