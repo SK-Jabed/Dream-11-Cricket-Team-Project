af96adb9836726c46aa53166e634e666aa898d01
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-13 23:23:06 +0600

    Buttons Design and Selected Players JSX Component Updated

--- src/components/Main/Buttons/Buttons.jsx
+++ src/components/Main/Buttons/Buttons.jsx
@@ -1,36 +1,71 @@
 import AvailablePlayers from "../AvailablePlayers/AvailablePlayers";
 import PropTypes from "prop-types";
-import "./Buttons.css"
+import "./Buttons.css";
 import SelectedPlayers from "../../SelectedPlayers/SelectedPlayers";
 
-const Buttons = ({handleIsActiveBtn, isActive, addPlayerToQueue, playerQueue, handleRemovePlayer}) => {
-    
-    return (
-        <div className="container w-11/12 mx-auto flex flex-col  lg:mt-20">
-            <div className="flex flex-col md:flex-row gap-2 lg:gap-0 justify-between items-center">
-                {
-                isActive.player ? <h2 className="text-2xl lg:text-3xl font-bold">Available Players</h2> : <h2 className="text-2xl font-bold lg:text-3xl">Selected Players ({playerQueue.length}/6)</h2>
-                }
-                <div className="flex">                   
-                <button onClick={() => handleIsActiveBtn("available")} className={`${isActive.player ? "bg-[#E7FE29] text-[#131313] font-bold rounded-tl-lg rounded-bl-lg px-7 py-3 border border-[#1313131a] border-r-0" : "text-[#13131399] bg-white border border-[#1313131a] border-r-0 px-7 py-3 rounded-tl-lg rounded-bl-lg"}`}>Available</button>
+const Buttons = ({
+  handleIsActiveBtn,
+  isActive,
+  addPlayerToQueue,
+  playerQueue,
+  handleRemovePlayer,
+}) => {
+  return (
+    <div className="container w-11/12 mx-auto flex flex-col  lg:mt-20">
+      <div className="flex flex-col md:flex-row gap-2 lg:gap-0 justify-between items-center">
+        {isActive.player ? (
+          <h2 className="text-2xl lg:text-3xl font-bold">Available Players</h2>
+        ) : (
+          <h2 className="text-2xl font-bold lg:text-3xl">
+            Selected Players ({playerQueue.length}/6)
+          </h2>
+        )}
+        <div className="flex">
+          <button
+            onClick={() => handleIsActiveBtn("available")}
+            className={`${
+              isActive.player
+                ? "bg-[#E7FE29] text-[#131313] font-bold rounded-tl-lg rounded-bl-lg px-7 py-3 border border-[#1313131a] border-r-0"
+                : "text-[#13131399] bg-white border border-[#1313131a] border-r-0 px-7 py-3 rounded-tl-lg rounded-bl-lg"
+            }`}
+          >
+            Available
+          </button>
 
-                <button onClick={() => handleIsActiveBtn("selected")} className={`${isActive.player ? "text-[#13131399] bg-white border border-[#1313131a] border-l-0 px-7 py-3 rounded-tr-lg rounded-br-lg" : "bg-[#E7FE29] text-[#131313] font-bold px-7 py-3 border border-[#1313131a] border-l-0 rounded-br-lg rounded-tr-lg"}`}>Selected ({playerQueue.length})</button>
-                </div>
-            </div>
-            
-            {
-                isActive.player ? <AvailablePlayers addPlayerToQueue={addPlayerToQueue}></AvailablePlayers> : <SelectedPlayers playerQueue={playerQueue} handleRemovePlayer={handleRemovePlayer} handleIsActiveBtn={handleIsActiveBtn}></SelectedPlayers>           
-            }                     
-        </div>   
-    );
+          <button
+            onClick={() => handleIsActiveBtn("selected")}
+            className={`${
+              isActive.player
+                ? "text-[#13131399] bg-white border border-[#1313131a] border-l-0 px-7 py-3 rounded-tr-lg rounded-br-lg"
+                : "bg-[#E7FE29] text-[#131313] font-bold px-7 py-3 border border-[#1313131a] border-l-0 rounded-br-lg rounded-tr-lg"
+            }`}
+          >
+            Selected ({playerQueue.length})
+          </button>
+        </div>
+      </div>
+
+      {isActive.player ? (
+        <AvailablePlayers
+          addPlayerToQueue={addPlayerToQueue}
+        ></AvailablePlayers>
+      ) : (
+        <SelectedPlayers
+          playerQueue={playerQueue}
+          handleRemovePlayer={handleRemovePlayer}
+          handleIsActiveBtn={handleIsActiveBtn}
+        ></SelectedPlayers>
+      )}
+    </div>
+  );
 };
 
 Buttons.propTypes = {
-    handleIsActiveBtn: PropTypes.func,
-    isActive: PropTypes.object,
-    addPlayerToQueue: PropTypes.func,
-    playerQueue: PropTypes.object,
-    handleRemovePlayer: PropTypes.func
-}
+  handleIsActiveBtn: PropTypes.func,
+  isActive: PropTypes.object,
+  addPlayerToQueue: PropTypes.func,
+  playerQueue: PropTypes.object,
+  handleRemovePlayer: PropTypes.func,
+};
 
 export default Buttons;
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,46 +1,67 @@
 import PropTypes from "prop-types";
 
-const SelectedPlayers = ({playerQueue, handleRemovePlayer, handleIsActiveBtn}) => {
-    return (
-        <div>
-
-            <div >
-                {
-                    playerQueue.map((player, idx) => (
-                        <div key={idx} className="border-2 border-solid p-2 rounded-2xl flex justify-between items-center mb-2">
-                            <div className="flex items-center gap-2">
-                                <div className="flex items-center">
-                                <img className="w-28 h-28 border-1 p-1 rounded-2xl" src={player.player_image} alt="Selected Players Image"></img>                            
-                                </div>
-                                <div className="flex flex-col gap-2">
-                                    <h3 className="text-2xl font-bold text-[#131313]">{player.player_name}</h3>
-                                    <p className="text-lg font-semibold text-gray-600">{player.player_role}</p>
-                                    <p className="text-lg font-semibold text-gray-600">Price: {player.bidding_price}</p>
-                                </div>
-                            </div>
-                            <div onClick={() => handleRemovePlayer(player.player_id)} className="border-2 border-solid border-yellow-300 px-5 py-3 rounded-xl hover:bg-yellow-400">
-                            <button  className="flex items-center space-x-2">
-                                <a>Remove</a>
-                                <i className="fa-solid fa-trash text-red-500"></i>
-                                </button>
-                            </div>
-                        </div>
-                    ))
-                }
-
+const SelectedPlayers = ({
+  playerQueue,
+  handleRemovePlayer,
+  handleIsActiveBtn,
+}) => {
+  return (
+    <div>
+      <div>
+        {playerQueue.map((player, idx) => (
+          <div
+            key={idx}
+            className="border-2 border-solid p-2 rounded-2xl flex justify-between items-center mb-2"
+          >
+            <div className="flex items-center gap-2">
+              <div className="flex items-center">
+                <img
+                  className="w-28 h-28 border-1 p-1 rounded-2xl"
+                  src={player.player_image}
+                  alt="Selected Players Image"
+                ></img>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-2xl font-bold text-[#131313]">
+                  {player.player_name}
+                </h3>
+                <p className="text-lg font-semibold text-gray-600">
+                  {player.player_role}
+                </p>
+                <p className="text-lg font-semibold text-gray-600">
+                  Price: {player.bidding_price}
+                </p>
+              </div>
             </div>
-
-            <div className="mt-8 ">
-                <button onClick={() => handleIsActiveBtn("available")} className="bg-yellow-400 rounded-xl py-3 px-5 hover:bg-yellow-500"><a className="text-lg font-bold">Add More Player</a></button>
+            <div
+              onClick={() => handleRemovePlayer(player.player_id)}
+              className="border-2 border-solid border-yellow-300 px-5 py-3 rounded-xl hover:bg-yellow-400"
+            >
+              <button className="flex items-center space-x-2">
+                <a>Remove</a>
+                <i className="fa-solid fa-trash text-red-500"></i>
+              </button>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-8 ">
+        <button
+          onClick={() => handleIsActiveBtn("available")}
+          className="bg-yellow-400 rounded-xl py-3 px-5 hover:bg-yellow-500"
+        >
+          <a className="text-lg font-bold">Add More Player</a>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 SelectedPlayers.propTypes = {
-    handleIsActiveBtn: PropTypes.func,
-    playerQueue: PropTypes.object,
-    handleRemovePlayer: PropTypes.func
-}
+  handleIsActiveBtn: PropTypes.func,
+  playerQueue: PropTypes.object,
+  handleRemovePlayer: PropTypes.func,
+};
 
 export default SelectedPlayers;
